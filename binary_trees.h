829e885e410fad2b3322b2a322dc9ba292792776
SFreire2022 <sebastiancazenave@gmail.com>
2022-07-20 14:59:11 -0300

    Add header betty compilant

--- binary_trees.h
+++ binary_trees.h
@@ -32,6 +32,7 @@ typedef struct binary_tree_s heap_t;
 /* Function Prototypes */
 /* Add node */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
 /* print bin tree */
 void binary_tree_print(const binary_tree_t *);
 
