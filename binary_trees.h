712d73e689b02269131519475e875a4f51537c22
SFreire2022 <sebastiancazenave@gmail.com>
2022-07-20 19:23:48 -0300

    Update function prototype is_root

--- binary_trees.h
+++ binary_trees.h
@@ -42,5 +42,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_delete(binary_tree_t *tree);
 /* Is leaf */
 int binary_tree_is_leaf(const binary_tree_t *node);
+/* Is root */
+int binary_tree_is_root(const binary_tree_t *node);
 
 #endif
