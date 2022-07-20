272f69a838909b0eedc4ddffa8504d0f0cbddc0e
SFreire2022 <sebastiancazenave@gmail.com>
2022-07-20 19:54:55 -0300

    Add all mandatory function prototypes

--- binary_trees.h
+++ binary_trees.h
@@ -44,5 +44,31 @@ void binary_tree_delete(binary_tree_t *tree);
 int binary_tree_is_leaf(const binary_tree_t *node);
 /* Is root */
 int binary_tree_is_root(const binary_tree_t *node);
+/* Preorder traversal */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+/*Inorder traversal */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
+/* Postorder traversal */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+/* Height measure */
+size_t binary_tree_height(const binary_tree_t *tree);
+/* Depth measure */
+size_t binary_tree_depth(const binary_tree_t *tree);
+/* Size measure */
+size_t binary_tree_size(const binary_tree_t *tree);
+/* Number of leaves */
+size_t binary_tree_leaves(const binary_tree_t *tree);
+/* Number of nodes with at least 1 child */
+size_t binary_tree_nodes(const binary_tree_t *tree);
+/* Balance Factor */
+int binary_tree_balance(const binary_tree_t *tree);
+/* Is full */
+int binary_tree_is_full(const binary_tree_t *tree);
+/* Is perfect */
+int binary_tree_is_perfect(const binary_tree_t *tree);
+/* Sibling of a node */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+/* Uncle of a node */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
 #endif
